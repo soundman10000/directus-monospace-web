4e0c17fdf57128f63b69ce0a6b33ff42ead1e0b6
Malley <jmalley@goempyrean.com>
2026-05-08 16:09:30 -0500

    cleanup bootstrap stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    monospaceApiKey: "eyJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSJ9.eyJhdWQiOiJhcGlLZXkiLCJqdGkiOiI4NTM0YmM3MS1lZjlkLTQ1MjctOWQ5NS05ZGNjMTc3MjU5ODciLCJpc3MiOiJEaXJlY3R1cyIsInN1YiI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCIsImlhdCI6MTc3ODI2MDI1MCwiZXhwIjoxODA5ODE3ODUwfQ.cbj-m2F0dDTFsfoPAqQiGCJKUiaVXjGIyEriZ5nlacRa1n--eUl3ZdlgfGf2bagJxeMkDaLnIFztRPhECgocBg",
+  },
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit'
+      ]
+    }
+  }
 })
