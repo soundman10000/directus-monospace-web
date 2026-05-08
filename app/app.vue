1d40d94eda345ddb4eabce7b0801b4ee348d3bc5
Malley <jmalley@goempyrean.com>
2026-05-08 14:27:02 -0500

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold text-gray-800 mb-4">Directus Monospace Web</h1>
+      <p class="text-lg text-gray-600">Nuxt project with Tailwind CSS</p>
+      <NuxtRouteAnnouncer />
+      <NuxtWelcome />
+    </div>
+  </div>
+</template>
